ed5864d64f8b00a5bc6dd9bedbb4823c72661392
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-01-27 13:27:25 +0100

    added name to button

--- com.examples.swingexample/src/main/java/com/examples/swingexample/MyAppWindow.java
+++ com.examples.swingexample/src/main/java/com/examples/swingexample/MyAppWindow.java
@@ -8,11 +8,17 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTable;
 
 public class MyAppWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private StudentRepository studentRepository;
+	private JTextField idTextField;
+	private JTextField nameTextField;
+	private JTable studentTable;
 
 	/**
 	 * Launch the application.
@@ -66,7 +72,7 @@ public class MyAppWindow extends JFrame {
 		setTitle(getClass().getSimpleName());
 		setBounds(100, 100, 450, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		getContentPane().setLayout(new GridLayout(4, 0, 0, 0));
 		
 		JButton btnClickMe = new JButton("Click me");
 		btnClickMe.setName("clickMe");
@@ -75,7 +81,23 @@ public class MyAppWindow extends JFrame {
 				studentRepository.save(new Student());
 			}
 		});
+		
+		JLabel lblId = new JLabel("id");
+		getContentPane().add(lblId);
+		
+		idTextField = new JTextField();
+		getContentPane().add(idTextField);
+		idTextField.setColumns(10);
+		
+		JLabel lblName = new JLabel("name");
+		getContentPane().add(lblName);
+		
+		nameTextField = new JTextField();
+		getContentPane().add(nameTextField);
+		nameTextField.setColumns(10);
 		getContentPane().add(btnClickMe);
+		
+		studentTable = new JTable();
+		getContentPane().add(studentTable);
 	}
-
 }
